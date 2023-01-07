fa7f8ce7dc9cedf72e11fc17a19b86a37aa4be7c
Suman Dhali <dhalisuman001@gmail.com>
2023-01-07 10:36:56 +0530

    navigation error

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,36 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import "../App.css";
+import BaseUrl from "../util/BaseUrl";
 
 const HomePage = () => {
   const navigate = useNavigate();
-  // const [userData, setUserData] = useState();
+  const [userData, setUserData] = useState();
   const logOut = (e) => {
     e.preventDefault();
     navigate("/login");
   };
-  // useEffect(() => {
-  //   fetch(`${BaseUrl}/auth/login/success`, {
-  //     method: "GET",
-  //     credentials: "include",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Credentials": true,
-  //     },
-  //   })
-  //     .then((response) => {
-  //       if (response.status === 200) return response.json();
-  //       throw new Error("authentication has been failed!");
-  //     })
-  //     .then((resObject) => {
-  //       setUserData(resObject.user);
-  //       console.log(resObject.user);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(`${BaseUrl}/auth/login/success`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        throw new Error("authentication has been failed!");
+      })
+      .then((resObject) => {
+        setUserData(resObject.user);
+        console.log(resObject.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="main-div">
@@ -49,7 +50,9 @@ const HomePage = () => {
         <button className="btn-l" onClick={logOut}>
           Logout
         </button>
-        <div id="typewriter">Welcome</div>
+        <div id="typewriter">
+          Welcome, {userData?.displayName || userData?.givenName}
+        </div>
         <br />
         <div className="about">
           <p>
