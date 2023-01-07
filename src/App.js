cf4f4f7c788ddf04ff287d7d01082f95e4b8284c
Suman Dhali <dhalisuman001@gmail.com>
2023-01-07 09:48:49 +0530

    pushed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index path="/" element={<Home />} />
-          <Route index path="/home" element={<HomePage />} />
+          <Route index path="/page" element={<HomePage />} />
           <Route index path="/login" element={<SignIn />} />
           <Route index path="/register" element={<SignUp />} />
         </Routes>
